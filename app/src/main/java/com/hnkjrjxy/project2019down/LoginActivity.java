13ac08bbedd2022e154d977010a639fdc06cfc02
xwx <1287212274@qq.com>
2019-06-19 20:48:00 +0800

    登录注册界面的基础原型

--- app/src/main/java/com/hnkjrjxy/project2019down/LoginActivity.java
+++ app/src/main/java/com/hnkjrjxy/project2019down/LoginActivity.java
@@ -1,12 +1,73 @@
 package com.hnkjrjxy.project2019down;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.hnkjrjxy.project2019down.activity.RegisterActivity;
 
 public class LoginActivity extends Activity {
+    private ImageView login_back;
+    private TextView login_ok;
+    private EditText login_phone;
+    private EditText login_pwd;
+    private TextView login_forget;
+    private TextView register_new;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+        initView();
+    }
+
+    private void initView() {
+        login_back = (ImageView) findViewById(R.id.login_back);
+        login_ok = (TextView) findViewById(R.id.login_ok);
+        login_phone = (EditText) findViewById(R.id.login_phone);
+        login_pwd = (EditText) findViewById(R.id.login_pwd);
+        login_forget = (TextView) findViewById(R.id.login_forget);
+        register_new = (TextView) findViewById(R.id.register_new);
+
+        register_new.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        login_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+
+    private void submit() {
+        // validate
+        String phone = login_phone.getText().toString().trim();
+        if (TextUtils.isEmpty(phone)) {
+            Toast.makeText(this, "输入手机号", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String pwd = login_pwd.getText().toString().trim();
+        if (TextUtils.isEmpty(pwd)) {
+            Toast.makeText(this, "输入密码", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // TODO validate success, do something
+
+
     }
 }
--- app/src/main/java/com/hnkjrjxy/project2019down/activity/RegisterActivity.java
+++ app/src/main/java/com/hnkjrjxy/project2019down/activity/RegisterActivity.java
@@ -1,9 +1,100 @@
 package com.hnkjrjxy.project2019down.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class RegisterActivity extends Activity {
-    //我写的第一个注释
-    //你好
-    //大家好才是真的好
+import com.hnkjrjxy.project2019down.R;
+
+public class RegisterActivity extends Activity implements View.OnClickListener {
+    private ImageView register_back;
+    private TextView register_ok;
+    private EditText register_phone;
+    private EditText register_authcode;
+    private Button register_btn_get;
+    private EditText register_pwd;
+    private ImageView register_pwd_look;
+    private boolean isLook = false;
+    private boolean isGet = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+        initView();
+    }
+
+    private void initView() {
+        register_back = (ImageView) findViewById(R.id.register_back);
+        register_ok = (TextView) findViewById(R.id.register_ok);
+        register_phone = (EditText) findViewById(R.id.register_phone);
+        register_authcode = (EditText) findViewById(R.id.register_authcode);
+        register_btn_get = (Button) findViewById(R.id.register_btn_get);
+        register_pwd = (EditText) findViewById(R.id.register_pwd);
+        register_pwd_look = (ImageView) findViewById(R.id.register_pwd_look);
+
+        register_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        register_pwd_look.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isLook = !isLook;
+                register_pwd_look.setImageResource(
+                        isLook ? R.mipmap.openeye : R.mipmap.closeeye);
+
+                register_pwd.setInputType(
+                        isLook ? InputType.TYPE_CLASS_TEXT : InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            }
+        });
+
+        register_btn_get.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.register_btn_get:
+                if(isGet){
+
+                }
+                break;
+        }
+    }
+
+    private void submit() {
+        // validate
+        String phone = register_phone.getText().toString().trim();
+        if (TextUtils.isEmpty(phone)) {
+            Toast.makeText(this, "输入手机号", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String authcode = register_authcode.getText().toString().trim();
+        if (TextUtils.isEmpty(authcode)) {
+            Toast.makeText(this, "输入验证码", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String pwd = register_pwd.getText().toString().trim();
+        if (TextUtils.isEmpty(pwd)) {
+            Toast.makeText(this, "输入验证码", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // TODO validate success, do something
+
+
+    }
 }
