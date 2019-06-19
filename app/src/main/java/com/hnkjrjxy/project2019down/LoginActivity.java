6c27fbf21c1970cfa5ad1c7091247e5e8ad4ef0c
xwx <1287212274@qq.com>
2019-06-19 20:14:43 +0800

    添加了图标

--- app/src/main/java/com/hnkjrjxy/project2019down/LoginActivity.java
+++ app/src/main/java/com/hnkjrjxy/project2019down/LoginActivity.java
@@ -1,6 +1,12 @@
 package com.hnkjrjxy.project2019down;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
 }
