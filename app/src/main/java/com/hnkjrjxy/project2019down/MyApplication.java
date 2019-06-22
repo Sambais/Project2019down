6aac2617ba00bd4c574e02566054a14adb21e697
xwx <1287212274@qq.com>
2019-06-22 18:10:55 +0800

    添加了一些变量

--- app/src/main/java/com/hnkjrjxy/project2019down/MyApplication.java
+++ app/src/main/java/com/hnkjrjxy/project2019down/MyApplication.java
@@ -2,12 +2,32 @@ package com.hnkjrjxy.project2019down;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.SharedPreferences;
 
 import com.hnkjrjxy.project2019down.util.WebSocketClient;
 
 public class MyApplication extends Application {
     public static Context context;
     public static String token;
+    public static int id;
+    public static boolean isLogin = false;
+    public static SharedPreferences sharedPreferences;
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        MyApplication.id = id;
+    }
+
+    public static boolean isIsLogin() {
+        return isLogin;
+    }
+
+    public static void setIsLogin(boolean isLogin) {
+        MyApplication.isLogin = isLogin;
+    }
 
     public static Context getContext() {
         return context;
@@ -29,5 +49,10 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         context = getApplicationContext();
+        sharedPreferences = getSharedPreferences("project2019",MODE_PRIVATE);
+        if(sharedPreferences.getInt("id",0) != 0){
+            id = sharedPreferences.getInt("id",0);
+            setIsLogin(true);
+        }
     }
 }
