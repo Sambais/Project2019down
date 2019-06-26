c06fdb8808f839041ca999c0d227b0c1877ec685
Jam <Jam@example.com>
2019-06-26 18:05:44 +0800

    画质选择

--- app/src/main/java/com/hnkjrjxy/project2019down/activity/SendPostActivity.java
+++ app/src/main/java/com/hnkjrjxy/project2019down/activity/SendPostActivity.java
@@ -15,6 +15,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
@@ -185,6 +186,7 @@ public class SendPostActivity extends Activity {
                     ToastUtil.toToast("请选择要分享到的频道");
                     return;
                 }
+                //正常画质
                 if (huazhi.equals("正常")){
                     //设置要展示的图片列表url集合
 //            Log.i(TAG, "onActivityResult: "+BitmapUtil.getRealPath(result.get(0),this));
@@ -192,10 +194,13 @@ public class SendPostActivity extends Activity {
                         img += BitmapUtil.bitmapToBase64(PhotoUtil.compressImageUpload(BitmapUtil.getRealPath(result.get(i),SendPostActivity.this)));
                     }
                 }else {
+                    //原图画质
                     for (int i = 0; i < result.size(); i++) {
                         img += BitmapUtil.bitmapToBase64(BitmapUtil.getRealPath(result.get(i),SendPostActivity.this));
                     }
                 }
+                //保持屏幕常亮，以防数据中断
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
                 JsonObject jsonObject = new JsonObject();
                 jsonObject.addProperty("img", img);
